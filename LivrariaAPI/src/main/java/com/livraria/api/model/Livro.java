8e932f0d3c6254cea501cb1de3531f0c4085cea0
Hugo Santos <hugosantos.ufg@gmail.com>
2025-07-20 19:27:22 -0300

    feito model livros

---
+++ LivrariaAPI/src/main/java/com/livraria/api/model/Livro.java
@@ -0,0 +1,24 @@
+package com.livraria.api.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collection = "livros")
+@Data
+public class Livro {
+
+    @Id
+    private String id;
+
+    private String isbn;
+    private String titulo;
+    private int anoPublicacao;
+    private String edicao;
+    private int numPaginas;
+    private String sinopse;
+
+    private List<String> autores;
+    private String categoria;
+}
